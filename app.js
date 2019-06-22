108e7b7790a7fcf6dfd4934352049a5d8d983a31
dingxuan.yao <tonyyao95@gmail.com>
2019-06-21 21:27:09 -0400

    Changing file structure

--- app.js
+++ app.js
@@ -17,26 +17,18 @@ app.get('/', (req, res)=>{
         res.render('index');
 });
 
-app.get('/about', (req, res)=>{
-        res.render('about');
-});
-
-app.get('/projects', (req, res)=>{
-        res.render('projects');
-});
-
 app.get('/contact', (req, res)=>{
         res.render('contact');
 });
 
-app.get('/scores', (req, res)=>{
-        con.query("SELECT idscore,name, score, ts FROM nodeSnakeScores.score ORDER BY score DESC LIMIT 10;", function (err,results,fields) {
-            // console.log(results);
-            res.render('scores',{dbscores: results});
-        });
+app.get('/snake/scores', (req, res)=>{
+    con.query("SELECT idscore,name, score, ts FROM nodeSnakeScores.score ORDER BY score DESC LIMIT 10;", function (err,results,fields) {
+        // console.log(results);
+        res.render('snake/scores',{dbscores: results});
     });
+});
 
-    app.get('/submitscore/:name/:score', (req, res)=>{
+app.get('/submitscore/:name/:score', (req, res)=>{
     queryStr = `INSERT INTO nodeSnakeScores.score (name, score) VALUES ("${req.params.name}", ${req.params.score});`;
     console.log(queryStr);
     con.query(queryStr, function (err,results,fields) {
@@ -49,20 +41,18 @@ app.get('/scores', (req, res)=>{
 });
 
 app.get('/submitscore/:name/:score', (req, res)=>{
-
-        queryStr = `INSERT INTO nodeSnakeScores.score (name, score) VALUES ("${req.params.name}", ${req.params.score});`;
-        console.log(queryStr);
-        con.query(queryStr, function (err,results,fields) {
-            // console.log(results);
-            if (err){
-                throw (err);
-            }
-            res.send('Score submitted');
-        });
+    queryStr = `INSERT INTO nodeSnakeScores.score (name, score) VALUES ("${req.params.name}", ${req.params.score});`;
+    console.log(queryStr);
+    con.query(queryStr, function (err,results,fields) {
+        // console.log(results);
+        if (err){
+            throw (err);
+        }
+        res.send('Score submitted');
     });
+});
 
 app.get('/deletescore/:id', (req, res)=>{
-// console.log(dateTime);
 queryStr = `DELETE FROM nodeSnakeScores.score WHERE idscore="${req.params.id}";`;
 console.log(queryStr);
 con.query(queryStr, function (err,results,fields) {
@@ -74,12 +64,13 @@ con.query(queryStr, function (err,results,fields) {
 });
 })
 
-app.get('/snake', (req, res)=>{
-        con.query("SELECT idscore,name, score, ts FROM nodeSnakeScores.score ORDER BY score DESC LIMIT 10;", function (err,results,fields) {
-            // console.log(results);
-            res.render('snake',{dbscores: results});
-        });
-    });
+
     
+app.get('/snake', (req, res)=>{
+    con.query("SELECT idscore,name, score, ts FROM nodeSnakeScores.score ORDER BY score DESC LIMIT 10;", function (err,results,fields) {
+        // console.log(results);
+        res.render('snake/index',{dbscores: results});
+    });
+});
 
 app.listen(80);
