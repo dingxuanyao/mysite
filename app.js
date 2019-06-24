239ab7b5bb6ec76cb19b23a769c4913398edd4a9
dingxuan.yao <tonyyao95@gmail.com>
2019-06-24 00:03:30 -0400

    small server tweak

--- app.js
+++ app.js
@@ -38,16 +38,6 @@ app.get('/submitscore/:name/:score', (req, res)=>{
     });
 });
 
-app.get('/submitscore/:name/:score', (req, res)=>{
-    queryStr = `INSERT INTO nodeSnakeScores.score (name, score) VALUES ("${req.params.name}", ${req.params.score});`;
-    con.query(queryStr, function (err,results,fields) {
-        if (err){
-            throw (err);
-        }
-        res.send('Score submitted');
-    });
-});
-
 app.get('/deletescore/:id', (req, res)=>{
     queryStr = `DELETE FROM nodeSnakeScores.score WHERE idscore="${req.params.id}";`;
     con.query(queryStr, function (err,results,fields) {
@@ -58,8 +48,6 @@ app.get('/deletescore/:id', (req, res)=>{
     });
 })
 
-
-    
 app.get('/snake', (req, res)=>{
     con.query("SELECT idscore,name, score, ts FROM nodeSnakeScores.score ORDER BY score DESC LIMIT 10;", function (err,results,fields) {
         res.render('snake/index',{dbscores: results});
